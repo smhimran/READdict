f3bf81b9b19829d29d16781ad9aa08c97f66d2a1
Shah Habibul Imran <smhimran@gmail.com>
2020-07-18 20:53:29 +0600

    Initial Commit

---
+++ READdict/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for READdict project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'READdict.settings')
+
+application = get_wsgi_application()
