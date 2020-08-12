9195456f67f8949ed48e1d1865d3ce39e5d05eab
Shah Habibul Imran <smhimran@gmail.com>
2020-08-12 20:27:24 +0600

    Added template files

--- Reading/views.py
+++ Reading/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'Reading/index.html')
+
+def books(request):
+    return render(request, 'Reading/shop-grid.html')
